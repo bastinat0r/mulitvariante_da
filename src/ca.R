bf237eb6c6c6eff4eda8ba8dbe80c9d8edc9ff6e
Sebastian Mai <bastinat0r@bastinat0r.de>
2014-05-16 22:26:30 +0200

    changing time format

--- src/ca.R
+++ src/ca.R
@@ -10,13 +10,20 @@ summary(fit)
 # Einlesen des Datensatzes (rp13 WLAN-Accespoints)
 rp13 <- read.csv("data/rp13.csv")
 
+# Zeitformat mit hinreichender genauigkeit. %F ~ y-m-d
+time_format <- "%FT%H"
+time <- strptime(rp13$Zeit, format = time_format)
+rp13$Zeit_re <- strftime(time, format = "%a%H")
+
+
 # Erstellen einer Kreuztabelle. Wichtig für ca: prop.t = T sorgt dafür, dass eine normalisierte Tabelle erstellt wird
-ct  <- crosstab(rp13$Zeit, rp13$Raum, prop.t = T, plot = F)
+ct  <- crosstab(rp13$Zeit_re, rp13$Raum, prop.t = T, plot = F)
 
 # Erstellen der ca
 fit <- ca(ct$prop.tbl)
 
 # Ausgabe als Graphik, Parameter siehe help(plot.ca)
-plot(fit, mass = c(F,T), contrib = "absolute", arrows = c(F, T), labels = c(0,2))
+plot(fit, mass = c(F,T), contrib = "absolute", arrows = c(F, T), labels = c(2,2))
 
+# Formatierte Textausgabe
 summary(fit)
