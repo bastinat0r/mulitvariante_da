9dcf5fd683931b50d67384945f8523a474affd67
Sebastian Mai <bastinat0r@bastinat0r.de>
2014-05-18 19:46:48 +0200

    diffrent things

--- src/ca.R
+++ src/ca.R
@@ -13,7 +13,7 @@ rp13 <- read.csv("data/rp13.csv")
 # Zeitformat mit hinreichender genauigkeit. %F ~ y-m-d
 time_format <- "%FT%H"
 time <- strptime(rp13$Zeit, format = time_format)
-rp13$Zeit_re <- strftime(time, format = "%a%H")
+rp13$Zeit_re <- strftime(time, format = "%H")
 
 
 # Erstellen einer Kreuztabelle. Wichtig für ca: prop.t = T sorgt dafür, dass eine normalisierte Tabelle erstellt wird
@@ -23,7 +23,26 @@ ct  <- crosstab(rp13$Zeit_re, rp13$Raum, prop.t = T, plot = F)
 fit <- ca(ct$prop.tbl)
 
 # Ausgabe als Graphik, Parameter siehe help(plot.ca)
-plot(fit, mass = c(F,T), contrib = "absolute", arrows = c(F, T), labels = c(2,2))
+plot(fit, mass = c(F,T), contrib = "absolute", arrows = c(F, T), labels = c(1,2))
 
 # Formatierte Textausgabe
 summary(fit)
+
+# Gerät & Raum
+ct2  <- crosstab(rp13$fortlaufende.Gerätenummer, rp13$Raum, prop.t = T, plot = F)
+fit2 <- ca(ct2$prop.tbl)
+plot(fit2, mass = c(F,T), contrib = "absolute", arrows = c(F, T), labels = c(0,2))
+
+# Räume ohne Garderobe und vip
+#rp13_subset <- subset(rp13, rp13$Raum != "garderobe" & rp13$Raum != "vip")
+
+# Bühnen & Workshop-Räume
+#rp13_subset <- subset(rp13, grepl("^(stage|workshop)", rp13$Raum))
+
+# siehe help(regex) und help(grepl)
+rp13_subset <- subset(rp13, !grepl("^(stage|workshop|garderobe|vip|lounge)", rp13$Raum))
+
+ct2_subset  <- CrossTable(rp13_subset$fortlaufende.Gerätenummer, rp13_subset$Raum)
+fit2_subset <- ca(ct2_subset$prop.tbl)
+plot(fit2_subset, mass = c(F,F), contrib = "absolute", arrows = c(F, T), labels = c(0,2))
+
